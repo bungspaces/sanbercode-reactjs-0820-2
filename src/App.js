fc8c755950adca5953c551c0b2f62683a96557ef
Gilang Segara Bening <enambk@gmail.com>
2020-09-04 22:21:09 +0700

    Tugas-9 (Week 2)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+  
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
+      {/**
+       * 
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -18,6 +21,44 @@ function App() {
         >
           Learn React
         </a>
+      </header>
+       */}
+      <header class="content">
+        <div class="border">
+          <h2>Form Pembelian Buah</h2>
+          <div class="justify">
+            Nama Pelanggan :&nbsp;
+            <input type="text" />
+          </div>
+          <div class="justify">
+            <div class="justify left">
+              <input type="radio" />
+              Semangka
+              <br />
+              <input type="radio" />
+              Jeruk
+              <br />
+              <input type="radio" />
+              Nanas
+              <br />
+              <input type="radio" />
+              Salak
+              <br />
+              <input type="radio" />
+              Anggur
+              <br />
+            </div>
+            <div class="justify">
+              Daftar Item :&nbsp;
+              <input class="left2" type="radio" />
+              Semangka
+              <br />
+            </div>
+          </div>
+          <div class="justify">
+            <input type="submit" class="mintul" value="kirim"></input>
+          </div>
+        </div>
       </header>
     </div>
   );
