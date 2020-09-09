0364f2e1a5cc8729cc425ce47c4444bbdf38dfac
Gilang Segara Bening <enambk@gmail.com>
2020-09-09 08:46:43 +0700

    Belum Implement Hook

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 /* import Tugas9 from "../src/Tugas-9/Tugas-9";
 import Tugas10 from "../src/Tugas-10/Tugas-10";
-import Tugas11 from "../src/Tugas-11/clock"; */
-import Tugas12 from "../src/Tugas-12/Tugas-12";
+import Tugas11 from "../src/Tugas-11/clock";
+import Tugas12 from "../src/Tugas-12/Tugas-12"; */
+import Tugas13 from "../src/Tugas-13/Tugas-13";
 
 function App() {
   return (
@@ -11,9 +12,11 @@ function App() {
 
       <Tugas10 />
 
-      <Tugas11 /> */}
+      <Tugas11 />
 
-      <Tugas12 />
+      <Tugas12 /> */}
+
+      <Tugas13 />
     </div>
   );
 }
