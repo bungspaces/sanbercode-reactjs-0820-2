dc63683f1679da16c183cd31214308302aee63d6
Gilang Segara Bening <enambk@gmail.com>
2020-09-16 10:48:58 +0700

    Module not found: Can't resolve '../src/Tugas-15/Routes' in 'C:\Users\enamb\sanbercode-reactjs-0820-2\src'

--- src/Tugas-15/Router.js
+++ src/Tugas-15/Router.js
@@ -3,11 +3,11 @@ import { Switch, Route } from 'react-router'
 import Switcher from './Switcher'
 
 // Component Tugas - Tugas
-import Form from '../Tugas9/Form'
-import ItemLists from '../Tugas-10/ItemLists'
-import Timer from '../Tugas-11/Timer'
-import ApiFetch from '../Tugas-13/apiFetch'
-import ListFruits from '../Tugas-13/ListFruits'
+import Tugas9 from '../Tugas-9/Tugas-9'
+import Tugas10 from '../Tugas-10/Tugas-10'
+import Tugas11 from '../Tugas-11/clock'
+import Tugas12 from '../Tugas-12/Tugas-12'
+import Tugas13 from '../Tugas-13/Tugas-13'
 import Fruit from '../Tugas-14/Buah'
 /* 
 import Tugas9 from "../src/Tugas-9/Tugas-9";
@@ -36,33 +36,33 @@ const Routes = () => {
                     <br />
                     <header><h1>Tugas 9</h1></header>
                     <br />
-                    <Form />
+                    <Tugas9 />
                 </Route>
                 <Route path='/Tugas-10' >
                     <br />
                     <header><h1>Tugas 10</h1></header>
                     <br />
-                    <ItemLists />
+                    <Tugas10 />
                 </Route>
                 <Route path='/Tugas-11'>
                     <br />
                     <header><h1>Tugas 11</h1></header>
                     <br />
-                    <Timer />
+                    <Tugas11 />
                 </Route>
                 <Route path='/Tugas-12'>
                     <br />
                     <header><h1>Tugas 12</h1></header>
                     <br />
-                    <ItemLists />
+                    <Tugas12 />
                 </Route>
                 <Route path='/Tugas-13'>
                     <br />
                     <header><h1>Tugas 13</h1></header>
                     <br />
-                    <ListFruits />
+                    <Tugas13 />
                     <br />
-                    <ApiFetch />
+                    <Tugas13 />
                 </Route>
                 <Route path='/Tugas-14'>
                     <br />
