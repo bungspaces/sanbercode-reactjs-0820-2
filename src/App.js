c69b651f1d323bfa3816e026e878a7764c42ec78
Gilang Segara Bening <enambk@gmail.com>
2020-09-16 10:31:01 +0700

    Tugas 15 Error

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
+import { Router } from "react-router-dom";
 /* import Tugas9 from "../src/Tugas-9/Tugas-9";
 import Tugas10 from "../src/Tugas-10/Tugas-10";
 import Tugas11 from "../src/Tugas-11/clock";
 import Tugas12 from "../src/Tugas-12/Tugas-12";
-import Tugas13 from "../src/Tugas-13/Tugas-13"; */
-import Tugas14 from "../src/Tugas-14/Buah.js";
+import Tugas13 from "../src/Tugas-13/Tugas-13";
+import Tugas14 from "../src/Tugas-14/Buah.js"; */
+import Tugas15 from  "../src/Tugas-15/Routes.js";
+
 
 function App() {
   return (
-    <div>
+    <Router>
 {/*       <Tugas9 />
 
       <Tugas10 />
@@ -17,10 +20,12 @@ function App() {
 
       <Tugas12 />
 
-      <Tugas13 /> */}
+      <Tugas13 />
 
-      <Tugas14 />
-    </div>
+      <Tugas14 /> */}
+
+      <Tugas15 />
+    </Router> 
   );
 }
 
--- src/Tugas-15/Router.js
+++ src/Tugas-15/Router.js
@@ -3,12 +3,20 @@ import { Switch, Route } from 'react-router'
 import Switcher from './Switcher'
 
 // Component Tugas - Tugas
-import Form from '../Tugas9/Form'
+/* import Form from '../Tugas9/Form'
 import ItemLists from '../Tugas10/ItemLists'
 import Timer from '../Tugas11/Timer'
 import ApiFetch from '../Tugas13/apiFetch'
 import ListFruits from '../Tugas13/ListFruits'
-import Fruit from '../Tugas14/Fruit'
+import Fruit from '../Tugas14/Fruit' */
+
+import Tugas9 from "../src/Tugas-9/Tugas-9";
+import Tugas10 from "../src/Tugas-10/Tugas-10";
+import Tugas11 from "../src/Tugas-11/clock";
+import Tugas12 from "../src/Tugas-12/Tugas-12";
+import Tugas13 from "../src/Tugas-13/Tugas-13";
+import Tugas14 from "../src/Tugas-14/Buah.js";
+import Tugas15 from  "../src/Tugas-15/Routes.js";
 
 const Home = () => {
     return (
@@ -24,31 +32,31 @@ const Routes = () => {
                 <Route exact path='/'>
                     <Home />
                 </Route>
-                <Route path='/Tugas9' >
+                <Route path='/Tugas-9' >
                     <br />
                     <header><h1>Tugas 9</h1></header>
                     <br />
                     <Form />
                 </Route>
-                <Route path='/Tugas10' >
+                <Route path='/Tugas-10' >
                     <br />
                     <header><h1>Tugas 10</h1></header>
                     <br />
                     <ItemLists />
                 </Route>
-                <Route path='/Tugas11'>
+                <Route path='/Tugas-11'>
                     <br />
                     <header><h1>Tugas 11</h1></header>
                     <br />
                     <Timer />
                 </Route>
-                <Route path='/Tugas12'>
+                <Route path='/Tugas-12'>
                     <br />
                     <header><h1>Tugas 12</h1></header>
                     <br />
                     <ItemLists />
                 </Route>
-                <Route path='/Tugas13'>
+                <Route path='/Tugas-13'>
                     <br />
                     <header><h1>Tugas 13</h1></header>
                     <br />
@@ -56,7 +64,7 @@ const Routes = () => {
                     <br />
                     <ApiFetch />
                 </Route>
-                <Route path='/Tugas14'>
+                <Route path='/Tugas-14'>
                     <br />
                     <header><h1>Tugas 14</h1></header>
                     <br />
